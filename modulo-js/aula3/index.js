ad2123f1e30dcdb3ebb729b101fa1a1fb86b2a40
Davi A Alves <daviih2000@gmail.com>
2025-04-24 14:43:38 -0300

    adiciona arquivos HTML e JavaScript com estrutura inicial

---
+++ modulo-js/aula3/index.js
@@ -0,0 +1,7 @@
+let numero1 = Number(prompt("Digite o primeiro número:"));
+let numero2 = Number(prompt("Digite o segundo número:"));
+let resultado = numero1 + numero2;
+// alert(resultado);
+document.write("O resultado é: " + resultado);
+
+console.log(resultado);
