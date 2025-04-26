be761a4d4488cdc08077954a9d75c56973cb3540
Davi A Alves <daviih2000@gmail.com>
2025-04-26 11:44:13 -0300

    adiciona selects para dia e mês e implementa função para preenchê-los

--- modulo-js/aula4/index.js
+++ modulo-js/aula4/index.js
@@ -1,5 +1,21 @@
-function boasVindas(nome) {
-  document.write(`<h1>Seja bem-vindo ao meu site, ${nome}!</h1>`);
+// function boasVindas(nome) {
+//   document.write(`<h1>Seja bem-vindo ao meu site, ${nome}!</h1>`);
+// }
+
+// boasVindas("Davi");
+
+// function somar(numero1, numero2) {
+//   return numero1 + numero2;
+// }
+
+// let resultado = somar(5, 10);
+// console.log(resultado);
+
+function preencherSelects(tag, limite) {
+  for (let i = 1; i < limite; i++) {
+    tag.innerHTML += `<option>${i}</option>`;
+  }
 }
 
-boasVindas("Davi");
+preencherSelects(dia, 32);
+preencherSelects(mes, 13);
