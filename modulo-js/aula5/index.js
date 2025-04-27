8d47fbdfd0e4750c97078b659675d53371226413
Davi A Alves <daviih2000@gmail.com>
2025-04-27 08:37:18 -0300

    adiciona arquivos HTML e JavaScript com estrutura inicial do projeto

---
+++ modulo-js/aula5/index.js
@@ -0,0 +1,14 @@
+let pessoa = {
+  nome: "Davi",
+  idade: 25,
+  altura: 1.8,
+  habilidades: ["JavaScript", "React", "Node.js"],
+  andar: function () {
+    console.log("Andando...");
+  },
+  rosto: {
+    olhos: "castanhos",
+    temCabelo: true,
+  },
+};
+ 
