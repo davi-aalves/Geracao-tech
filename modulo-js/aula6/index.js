8c9a51a8af2973344bd28d88cdddd42bc5bcb4b9
Davi A Alves <daviih2000@gmail.com>
2025-04-30 15:31:02 -0300

    altera o modo padrão para "light" e comenta a lógica do switch case

--- modulo-js/aula6/index.js
+++ modulo-js/aula6/index.js
@@ -1,20 +1,22 @@
 // Switch Case
 
-let mode = "dark";
+let mode = "light";
 
-switch (mode) {
-  case "dark":
-    document.querySelector("body").style.backgroundColor = "grey";
-    break;
+// switch (mode) {
+//   case "dark":
+//     document.querySelector("body").style.backgroundColor = "grey";
+//     break;
 
-  case "red":
-    document.querySelector("body").style.backgroundColor = "red";
-    break;
+//   case "red":
+//     document.querySelector("body").style.backgroundColor = "red";
+//     break;
 
-  case "yellow":
-    document.querySelector("body").style.backgroundColor = "yellow";
-    break;
+//   case "yellow":
+//     document.querySelector("body").style.backgroundColor = "yellow";
+//     break;
 
-  default:
-    document.querySelector("body").style.backgroundColor = "white";
-}
+//   default:
+//     document.querySelector("body").style.backgroundColor = "white";
+// }
+
+console.log(mode == "dark" ? "Dark Mode: On" : "Dark Mode: Off");
