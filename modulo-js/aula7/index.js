52fefb470a0393fcd4f00b346e4febb68c040d24
Davi A Alves <daviih2000@gmail.com>
2025-05-02 17:52:25 -0300

    adiciona arquivos HTML e JavaScript com estrutura inicial para aula de estruturas de repetição e arrays

---
+++ modulo-js/aula7/index.js
@@ -0,0 +1,13 @@
+let nome = "Lucas";
+let nome1 = "Pedro";
+let nome2 = `João`;
+let nome3 = "Maria";
+
+let nomes = ["Lucas", "Pedro", "João", "Maria"];
+
+let numeros = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < numeros.length; i++) {
+  numeros[i] += 1;
+  console.log("volta: ", i);
+}
