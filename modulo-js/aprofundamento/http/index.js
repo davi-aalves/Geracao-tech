a0f6ae2be0076b39f2bbddeff24d0eab0736c2ec
Davi A Alves <daviih2000@gmail.com>
2025-05-08 18:01:47 -0300

    remove formulário de CEP e implementa seleção de regiões e estados

--- modulo-js/aprofundamento/http/index.js
+++ modulo-js/aprofundamento/http/index.js
@@ -1,12 +1,38 @@
-cep.onkeyup = () => {
-  if (cep.value.length === 8) {
-    fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
-      .then((resposta) => resposta.json())
-      .then((resposta2) => {
-        rua.value = resposta2.logradouro;
-        bairro.value = resposta2.bairro;
-        cidade.value = resposta2.localidade;
-        estado.value = resposta2.estado;
+// cep.onkeyup = () => {
+//   if (cep.value.length === 8) {
+//     fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+//       .then((resposta) => resposta.json())
+//       .then((resposta2) => {
+//         rua.value = resposta2.logradouro;
+//         bairro.value = resposta2.bairro;
+//         cidade.value = resposta2.localidade;
+//         estado.value = resposta2.estado;
+//       });
+//   }
+// };
+
+function buscarRegioes() {
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/regioes")
+    .then((res) => res.json())
+    .then((res) => {
+      res.map((regiao) => {
+        regioes.innerHTML += `<option value="${regiao.id}">${regiao.nome}</option>`;
       });
-  }
-};
+    });
+}
+
+function buscarEstados() {
+  fetch(
+    `https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regioes.value}/estados`
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      estados.innerHTML = "";
+      res.map((estado) => {
+        estados.innerHTML += `<option value="${estado.id}">${estado.nome}</option>`;
+      });
+    });
+}
+
+buscarRegioes();
+buscarEstados();
