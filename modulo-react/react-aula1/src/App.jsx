53c35778790ed2b6e6c7d115cc9364f867e1468b
Davi A Alves <daviih2000@gmail.com>
2025-05-19 18:00:57 -0300

    feat: initialize React project with Vite
    - Add package.json with dependencies and scripts for development, build, linting, and preview.
    - Include Vite logo SVG in the public directory.
    - Create App.css for styling the application layout and components.
    - Implement a simple Hello component in App.jsx.
    - Add React logo SVG in assets directory.
    - Set up global styles in index.css for consistent theming and typography.
    - Create main entry point in main.jsx to render the App component.
    - Configure Vite with a basic setup in vite.config.js.

---
+++ modulo-react/react-aula1/src/App.jsx
@@ -0,0 +1,13 @@
+const Hello = () => {
+  return <h1>Olá Dev!</h1>;
+};
+
+const App = () => {
+  // parte lógica do componente
+  return (
+    // parte visível para o usuário
+    <Hello />
+  );
+};
+
+export default App;
